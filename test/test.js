a2aa1df0688316ef89787da05a8163548faed01b
Choi Kyung Seok <Daniel@Choi-Kyung-Seokui-MacBook-Pro.local>
2017-02-19 23:27:32 +0900

    테스트 케이스 최초 작성 with Should.js

---
+++ test/test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+require('should');
+
+describe('여행사 예약 시스템 서비스', function () {
+    it('테스트 케이스 최초 작성', function(){
+        const value = 10;
+        value.should.be.equal(10);
+    });
+});
